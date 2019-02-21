e90411471b9eb148f0c956601032f24d71de6c84
Hayden Parkinson <haydendparkinson@gmail.com>
2019-02-21 16:06:33 -0700

    making Database object like exampleDAO

--- src/main/java/database_access/DataAccessObject.java
+++
@@ -1,7 +0,0 @@
-package database_access;
-
-import java.io.File;
-
-public class DataAccessObject {
-    protected static String dbPath = "jdbc:sqlite:db"+ File.separator + "familymapserver.sqlite";
-}
